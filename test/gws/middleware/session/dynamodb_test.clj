61787f8632250db407a99c5efa57abcc07ac6417
Gordon Stratton <gordon.stratton@gmail.com>
2015-02-27 05:53:51 +0000

    Update tests

--- test/gws/middleware/session/dynamodb_test.clj
+++ test/gws/middleware/session/dynamodb_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [gws.middleware.session.dynamodb :as ddb]
             [ring.middleware.session.store :as store])
-  (:import [gws.middleware.session.dynamodb DynamoDBStore]))
+  (:import [gws.middleware.session.dynamodb DynamoDBStore]
+           [java.util UUID]))
 
 (def ^:private test-data
   {:map {:a 1 :b 2 :c 3 :d {:e 4 :f {:g 5 :h 6 :i 7}}}
@@ -10,27 +11,36 @@
    :vector [1 2 3 4 5 [6 7 8 [9 10]]]
    :vector-empty []})
 
-(def ^:private test-ddb-table-name
-  (str (java.util.UUID/randomUUID)))
+(def ^:private table
+  (str (UUID/randomUUID)))
 
-(def store (ddb/dynamodb-store {:table-name test-ddb-table-name}))
+(defn- store
+  [table]
+  (ddb/dynamodb-store {} {:table-name table}))
 
 (deftest create
-  (is (instance? DynamoDBStore store)))
+  (testing "Create fresh store"
+    (is (instance? DynamoDBStore (store table))))
+  (testing "dynamodb-store is idempotent"
+    (is (instance? DynamoDBStore (store table)))))
 
 (deftest read-writes
-  (is (= "42069" (store/write-session store "42069" test-data)))
-  (is (= test-data (store/read-session store "42069"))))
+  (let [store (store table)]
+    (is (= "42069" (store/write-session store "42069" test-data)))
+    (is (= test-data (store/read-session store "42069")))))
 
 (deftest delete-key
-  (is (= "42069" (store/write-session store "42069" test-data)))
-  (is (= nil (store/delete-session store "42069"))))
+  (let [store (store table)]
+    (is (= "42069" (store/write-session store "42069" test-data)))
+    (is (= nil (store/delete-session store "42069")))))
 
 (deftest delete-nonexistent-key
-  (is (= nil (store/delete-session store "something-i-never-added"))))
+  (let [store (store table)]
+    (is (= nil (store/delete-session store "something-i-never-added")))))
 
 (deftest write-with-nil-key
-  (let [id (store/write-session store nil test-data)]
+  (let [store (store table)
+        id (store/write-session store nil test-data)]
     (is (string? id))
     (is (= test-data (store/read-session store id)))
     (is (= nil (store/delete-session store id)))))
