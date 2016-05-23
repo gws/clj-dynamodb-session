ba08c08477cc547b211ebfaa11102ca0ee757500
Gordon Stratton <gordon.stratton@gmail.com>
2016-05-23 23:12:53 +0000

    v0.4.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gws/clj-dynamodb-session "0.4.2"
+(defproject gws/clj-dynamodb-session "0.4.3"
   :description "A DynamoDB-backed Ring session store"
   :url "https://gitlab.com/gws/clj-dynamodb-session"
   :min-lein-version "2.0.0"
