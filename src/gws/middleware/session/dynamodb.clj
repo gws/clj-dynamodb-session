7c992024849e233b008788be7fb44e065c8fab5d
Gordon Stratton <gordon.stratton@gmail.com>
2016-11-23 04:07:30 +0000

    Refactor session handler

--- src/gws/middleware/session/dynamodb.clj
+++ src/gws/middleware/session/dynamodb.clj
@@ -15,49 +15,34 @@
 (ns gws.middleware.session.dynamodb
   (:require [cognitect.transit :as transit]
             [ring.middleware.session.store :refer [SessionStore]])
-  (:import [com.amazonaws AmazonServiceException]
-           [com.amazonaws.regions Region
+  (:import [com.amazonaws.regions Region
                                   Regions]
            [com.amazonaws.services.dynamodbv2 AmazonDynamoDBClient]
            [com.amazonaws.services.dynamodbv2.model AttributeValue
-                                                    AttributeDefinition
-                                                    CreateTableRequest
-                                                    GetItemRequest
-                                                    KeySchemaElement
-                                                    ProvisionedThroughput]
-           [com.amazonaws.services.dynamodbv2.util TableUtils]
+                                                    GetItemRequest]
            [java.io ByteArrayInputStream
                     ByteArrayOutputStream]
            [java.nio ByteBuffer]
-           [java.text SimpleDateFormat]
-           [java.util Date
-                      TimeZone
-                      UUID]))
-
-(defrecord Options [client endpoint region table-name read-capacity-units
-                    write-capacity-units])
+           [java.util UUID]))
 
 (def default-options
-  "Default options suitable for testing DynamoDB locally."
+  "Default options for the DynamoDB store."
   {:client nil
-   :endpoint "http://localhost:8000"
    :region nil
-   :table-name "clj-dynamodb-sessions"
-   :read-capacity-units 5
-   :write-capacity-units 5})
+   :endpoint "http://localhost:8000"
+   :table-name "clj-dynamodb-session"
+   :id-attribute-name "id"
+   :data-attribute-name "data"
+   :custom-attributes {}})
 
-(def ^java.text.DateFormat date-format
-  (doto (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss'Z'")
-        (.setTimeZone (TimeZone/getTimeZone "UTC"))))
-
-(defn- decode
+(defn decode
   "Decodes a java.nio.ByteBuffer into a Clojure data structure."
   [^ByteBuffer data]
   (-> (ByteArrayInputStream. (.array data))
       (transit/reader :msgpack)
       (transit/read)))
 
-(defn- encode
+(defn encode
   "Encodes a Clojure data structure into a java.nio.ByteBuffer."
   ^java.nio.ByteBuffer
   [data]
@@ -65,70 +50,67 @@
     (transit/write (transit/writer baos :msgpack) data)
     (ByteBuffer/wrap (.toByteArray baos))))
 
-(defn- region-from-string
-  "Returns a Region object given a string representation of an AWS region."
-  ^com.amazonaws.regions.Region
-  [region]
-  (Region/getRegion (Regions/fromName region)))
-
-(defn- create-session-table!
-  "Create the DynamoDB table which will hold session data."
-  [^AmazonDynamoDBClient client options]
-  (let [request (CreateTableRequest.
-                  [(AttributeDefinition. "id" "S")]
-                  (:table-name options)
-                  [(KeySchemaElement. "id" "HASH")]
-                  (ProvisionedThroughput. (:read-capacity-units options)
-                                          (:write-capacity-units options)))]
-    (let [_ (TableUtils/createTableIfNotExists client request)]
-      (TableUtils/waitUntilActive client (:table-name options)))))
-
 (deftype DynamoDBStore [^AmazonDynamoDBClient client options]
   SessionStore
 
   (read-session [store key]
     (when key
-      (let [item {"id" (.withS (AttributeValue.) key)}
-            request (-> (GetItemRequest. (:table-name options) item true)
-                        (.withExpressionAttributeNames {"#d" "data"})
-                        (.withProjectionExpression "#d"))
-            result (.getItem client request)]
-        (when-let [item (.getItem result)]
-          (when-let [data (.getB ^AttributeValue (get item "data"))]
-            (decode data))))))
+      (let [{id-name :id-attribute-name
+             data-name :data-attribute-name
+             table-name :table-name}
+            options
+
+            pk {id-name (AttributeValue. (str key))}
+            request (-> (GetItemRequest. table-name pk true)
+                        (.withProjectionExpression "#d")
+                        (.withExpressionAttributeNames {"#d" data-name}))]
+        (when-let [item (.getItem (.getItem client request))]
+          (-> ^AttributeValue (get item data-name)
+              (.getB)
+              (decode))))))
   (write-session [store key data]
-    (let [key (or key (str (UUID/randomUUID)))
-          now-str (.format date-format (Date.))
-          item {"id" (.withS (AttributeValue.) key)
-                "data" (.withB (AttributeValue.) (encode data))
-                "updated_at" (.withS (AttributeValue.) now-str)}]
-      (.putItem client (:table-name options) item)
+    (let [{id-name :id-attribute-name
+           data-name :data-attribute-name
+           table-name :table-name
+           custom-attrs :custom-attributes}
+          options
+
+          key (str (or key (UUID/randomUUID)))
+          item (into {id-name (AttributeValue. key)
+                      data-name (.withB (AttributeValue.) (encode data))}
+                     (map (fn [[k v]] [k (v data)]) custom-attrs))]
+      (.putItem client table-name item)
       key))
   (delete-session [store key]
     (when key
-      (let [item {"id" (.withS (AttributeValue.) key)}]
-        (.deleteItem client (:table-name options) item)
+      (let [{id-name :id-attribute-name
+             table-name :table-name}
+            options
+
+            item {id-name (AttributeValue. (str key))}]
+        (.deleteItem client table-name item)
         nil))))
 
 (defn dynamodb-client
-  "Creates an AmazonDynamoDBClient from an options map."
-  ^com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+  "Creates an AmazonDynamoDBClient from the session store options map."
   [options]
-  (let [options (map->Options (merge default-options options))
-        client (or ^AmazonDynamoDBClient (:client options)
-                   (AmazonDynamoDBClient.))]
-    (if (:region options)
-      (.withRegion client (region-from-string (:region options)))
-      (.withEndpoint client (:endpoint options)))))
+  {:post [(instance? AmazonDynamoDBClient %)]}
+  (if-let [client (:client options)]
+    client
+    (let [client (AmazonDynamoDBClient.)]
+      (if (:region options)
+        (.withRegion client (-> (:region options)
+                                (Regions/fromName)
+                                (Region/getRegion)))
+        (.withEndpoint client (:endpoint options))))))
 
 (defn dynamodb-store
   "Create a new DynamoDB session store implementing the SessionStore protocol.
 
-  See default-options for example options."
+  See `default-options` for example options."
   ([]
    (dynamodb-store {}))
   ([options]
-   (let [options (map->Options (merge default-options options))
+   (let [options (merge default-options options)
          client (dynamodb-client options)]
-     (create-session-table! client options)
      (DynamoDBStore. client options))))
