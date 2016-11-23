1bcd25357bb669d6da69e5f50e9cceea053eba26
Gordon Stratton <gordon.stratton@gmail.com>
2016-11-23 04:13:46 +0000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,14 @@
   :min-lein-version "2.0.0"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[com.amazonaws/aws-java-sdk-core "1.11.49"
+  :dependencies [[com.amazonaws/aws-java-sdk-core "1.11.60"
                   :exclusions [commons-codec
                                joda-time]]
-                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.49"
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.60"
                   :exclusions [commons-logging]]
-                 [com.cognitect/transit-clj "0.8.290"]
+                 [com.cognitect/transit-clj "0.8.295"]
                  [ring/ring-core "1.5.0"]]
   :global-vars {*warn-on-reflection* true}
-  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]
+                   :plugins [[jonase/eastwood "0.2.3"]]}
              :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}})
